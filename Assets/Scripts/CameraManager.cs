bf7bf1b8972abfdbcd4c8fdb0815e275fe004f98
bajuwa <justcallmebaj@gmail.com>
2014-10-12 21:07:56 -0700

    Changed grab and drag style panning to camera edge detection panning.  The camera also moves now instead of the map

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -9,8 +9,7 @@ public class CameraManager : MonoBehaviour {
 	private GameObject mapManager;
 	private GameObject mapImage;
 	
-	private Vector3 rightClickCoord;
-	private Vector3 oldTileCoord;
+	private float cameraSpeed = 0.3f;
 	private float camMinX;
 	private float camMaxX;
 	private float camMinY;
@@ -30,24 +29,16 @@ public class CameraManager : MonoBehaviour {
 		mainCam.orthographicSize = Mathf.Clamp(mainCam.orthographicSize, minOrthoSize, maxOrthoSize);
 			
 		// Update map location when holding and dragging right mouse button
-		Vector3 tempMapPosition = mapManager.transform.position;
+		Vector3 tempCamPosition = Camera.main.transform.position;
 		
-		// Note: 1 stands for 'right mouse button'
-		// First keep track of where the player first clicked down
-		if (Input.GetMouseButtonDown(1)) {
-			rightClickCoord = mainCam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
-			oldTileCoord = mapManager.transform.position;
-		}
-		
-		// Update the Maps position while playing is holding down the right mouse button
-		if (Input.GetMouseButton(1)) {
+		// Update the Maps position while cursor is against the edge of the camera view
+		if (mouseIsAgainstCameraEdge()) {
 			// Then update the map position by how far the player moved their mouse while holding down
-			Vector3 tempMousePosition = mainCam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+			Vector3 tempMousePosition = mainCam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
 			
-			// Adjust map position by the distance the mouse has moved
-			tempMapPosition.x = oldTileCoord.x - (rightClickCoord.x - tempMousePosition.x);
-			tempMapPosition.y = oldTileCoord.y - (rightClickCoord.y - tempMousePosition.y);
-			mapManager.transform.position = tempMapPosition;
+			// Adjust map position away from the mouse (since we move the map, not the camera)
+			// Note: Moving 'towards' a point with negative speed actually moves it away, which is what we want
+			tempCamPosition = Vector3.MoveTowards(tempCamPosition, tempMousePosition, cameraSpeed);
 		}
 		
 		// Since we will need to enforce the bounds of the camera, get the camera bounds info
@@ -58,17 +49,33 @@ public class CameraManager : MonoBehaviour {
 		
 		// Ensure the player does not pan the camera past the limits of the maps edges
 		if (camMinX < mapImage.renderer.bounds.min.x) {
-			tempMapPosition.x -= mapImage.renderer.bounds.min.x - camMinX;
+			tempCamPosition.x += mapImage.renderer.bounds.min.x - camMinX;
 		}
 		if (camMinY < mapImage.renderer.bounds.min.y) {
-			tempMapPosition.y -= mapImage.renderer.bounds.min.y - camMinY;
+			tempCamPosition.y += mapImage.renderer.bounds.min.y - camMinY;
 		}
 		if (camMaxX > mapImage.renderer.bounds.max.x) {
-			tempMapPosition.x += camMaxX - mapImage.renderer.bounds.max.x;
+			tempCamPosition.x -= camMaxX - mapImage.renderer.bounds.max.x;
 		}
 		if (camMaxY > mapImage.renderer.bounds.max.y) {
-			tempMapPosition.y += camMaxY - mapImage.renderer.bounds.max.y;
+			tempCamPosition.y -= camMaxY - mapImage.renderer.bounds.max.y;
 		}
-		mapManager.transform.position = tempMapPosition;
+		
+		tempCamPosition.z = Camera.main.transform.position.z;
+		Camera.main.transform.position = tempCamPosition;
+	}
+	
+	private bool mouseIsAgainstCameraEdge() {
+		Vector2 mousePos = Input.mousePosition;
+		
+		// If the mouse is in the outer 10% of the screen, camera should pan
+		if (mousePos.x > (Screen.width * 0.9f) || 
+			mousePos.x < (Screen.width * 0.1f) ||
+			mousePos.y > (Screen.height * 0.9f) || 
+			mousePos.y < (Screen.height * 0.1f)) {
+				return true;
+		}
+		
+		return false;
 	}
 }
