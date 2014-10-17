a9b12567c17fbaf0c850f093cd3eb3dcb1cdafb2
Zeraine <Zeraine@gmail.com>
2014-10-16 22:45:37 -0700

    Changed how Text works
    Using GUIStyle, made the text wrap and bold. It now has more of a
    appearance of being bounded in the box using hardcoded values.
    
    TODO:
    1. Do same with image to the left of it
    2. Remove hardcode for more reliable values that will be resolution safe

--- Assets/Scripts/GUIManager.cs
+++ Assets/Scripts/GUIManager.cs
@@ -4,27 +4,34 @@ using System.Collections;
 public class GUIManager : MonoBehaviour {
 	//MapUIManager object
 	public MapUIManager mUM;
+	
+	public GUITexture uiHead;
+	public GUITexture uiStatus;
 	//Empty GUITexture that will display what is selected in the bottom left
 	public GUITexture headDisplay;
 	//Empty GUIText that will display the description of the unit/tile
 	public GUIText statusDisplay;
 	// Use this for initialization
+	public GUIStyle descriptionStyle;
 	void Start () {
 		//Set it to null on runtime because the default is
 		//the unity logo.
 		headDisplay.texture = null;
 		statusDisplay.text = null;
+		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void OnGUI () {
 		// Get the currently selected object that we will be displaying information about
 		Selectable currentlySelected = mUM.getCurrentlySelectedObject();
 		if (currentlySelected) {
 			// Assign the texture a new texture based on what is selected and display it in the bottom left
 			headDisplay.texture = currentlySelected.displayImage;
 			// Assign the GUIText new text based on what is selected and display it next to the head image
-			statusDisplay.text = currentlySelected.description;
+			//statusDisplay.text = currentlySelected.description;
+			GUI.Label (new Rect (300,730,200,uiStatus.texture.height), currentlySelected.description, descriptionStyle);
 		}
 	}
+
 }
