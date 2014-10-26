ea93ca073d9439e420eedd5bdf03afc6c4bc7dd6
bajuwa <justcallmebaj@gmail.com>
2014-10-25 19:40:49 -0700

    Missed some new files

---
+++ Assets/Scripts/MapObjects/Attackable.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attackable : Selectable {
+	protected bool isInBattle = false;
+	
+	// Unit Stats (TODO: protect once done dev testing)
+	// When changing stats between different unit types, change them in the Prefab, not in any classes
+	public float currentHp = 10f;
+	public float maxHp = 10f;
+	public float attack = 1f;
+	public float defense = 2f;
+	
+	// Destroy this unit, making sure to destroy paths and selections
+	public void kill() {
+		Debug.Log("I was killed!");
+		this.deselect(GetInstanceID());
+		GameObject.Destroy(this.gameObject);
+	}
+	
+	public virtual void startBattle() {
+		Debug.Log("Another unit attacked me!");
+	}
+	
+	public virtual void removeFromBattle() {
+		Debug.Log("Done battling");
+	}
+	
+	public virtual void interrupt() {
+		Debug.Log("Interrupted!");
+	}
+
+}
