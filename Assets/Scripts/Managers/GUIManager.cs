e4bd20b7e66ca84d7862a14f742bc330f7b24da8
bajuwa <justcallmebaj@gmail.com>
2014-11-01 21:49:23 -0700

    Updated gummy bear bag and blueberry bush with new images

--- Assets/Scripts/Managers/GUIManager.cs
+++ Assets/Scripts/Managers/GUIManager.cs
@@ -22,7 +22,7 @@ public class GUIManager : MonoBehaviour {
 		headDisplay.texture = null;
 		statusDisplay.text = null;
 		
-		mUM = GameObject.Find("MapUIManager").GetComponent<MapUIManager>();
+		mUM = GameObject.Find("UIManager").GetComponent<MapUIManager>();
 	}
 	
 	void Update() {
