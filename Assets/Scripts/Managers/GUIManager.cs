91c93476252074e3783c580f136f96b1e4751eac
bajuwa <justcallmebaj@gmail.com>
2014-11-01 22:33:13 -0700

    Added stats to the display when selecting an ant unit

--- Assets/Scripts/Managers/GUIManager.cs
+++ Assets/Scripts/Managers/GUIManager.cs
@@ -26,6 +26,7 @@ public class GUIManager : MonoBehaviour {
 	}
 	
 	void Update() {
+		if (!mUM) mUM = GameObject.Find("UIManager").GetComponent<MapUIManager>();
 		currentlySelected = mUM.getCurrentlySelectedObject();
 	}
 	
@@ -36,8 +37,21 @@ public class GUIManager : MonoBehaviour {
 			// Assign the texture a new texture based on what is selected and display it in the bottom left
 			headDisplay.texture = currentlySelected.getDisplayImage();
 			// Assign the GUIText new text based on what is selected and display it next to the head image
-			//statusDisplay.text = currentlySelected.description;
 			GUI.Label (new Rect (220,480,200,uiStatus.texture.height), currentlySelected.getDescription(), descriptionStyle);
+			
+			// If the currently selected object is an ant unit, display its stats
+			AntUnit antUnitScript = currentlySelected.GetComponent<AntUnit>();
+			if (antUnitScript != null) {
+				string stats = string.Format(
+					"HP: {0:0.0}/{1}    Attack: {2}\nDefense: {3}    Speed: {4}", 
+					antUnitScript.currentHp, 
+					antUnitScript.maxHp,
+					antUnitScript.attack,
+					antUnitScript.defense,
+					antUnitScript.speed
+				);
+				GUI.Label (new Rect (240,530,200,20), stats, descriptionStyle);
+			}
 		}
 	}
 
