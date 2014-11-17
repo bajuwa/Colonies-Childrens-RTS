10d44bca952c6973a92526a29da08759a3fc1ecc
bajuwa <justcallmebaj@gmail.com>
2014-11-16 17:23:26 -0800

    Event icons will hug the edge of the screen and rotate to view their given event location

---
+++ Assets/Scripts/Managers/EventIcon.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventIcon : MonoBehaviour {
+
+    public Vector2 eventLocation;
+	
+	private Transform arrow;
+
+	// Use this for initialization
+	void Start () {
+		arrow = transform.Find("arrow");
+	}
+	
+	// Make sure we are constantly pointing at our event location
+	void Update () {
+		// Position the icon on the edge of the screen at the closest possible location to the actual event
+		Vector3 v3Screen = Camera.main.WorldToViewportPoint(eventLocation);
+		if (!(v3Screen.x > -0.01f && v3Screen.x < 1.01f && v3Screen.y > -0.01f && v3Screen.y < 1.01f)) {
+			v3Screen.x = Mathf.Clamp (v3Screen.x, 0.01f, 0.99f);
+			v3Screen.y = Mathf.Clamp (v3Screen.y, 0.01f, 0.99f);
+			transform.position = Camera.main.ViewportToWorldPoint (v3Screen);
+			transform.localPosition = new Vector3(
+				transform.localPosition.x,
+				transform.localPosition.y,
+				0
+			);
+		}
+		
+		// Rotate the arrow indicator to point at the location
+		float newAngle = SignedAngleBetween(transform.position, eventLocation)-90;
+		transform.rotation = Quaternion.Euler(new Vector3(0, 0, newAngle));
+	}
+	
+	private float SignedAngleBetween(Vector2 p1, Vector2 p2) {
+		return Mathf.Atan2(p2.y-p1.y, p2.x-p1.x)*180 / Mathf.PI;
+	}
+}
---
+++ Assets/Scripts/Managers/EventManager.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EventManager : MonoBehaviour {
+
+	public GameObject eventIconPrefab;
+	
+	private List<GameObject> events;
+	private List<GameObject> eventsToRemove;
+	
+	public void addEvent(Vector2 position) {
+		// If the event is already happening on screen, ignore the request
+		// (it would be deleted right afterwords anyways)
+		if (isOnScreen(position)) return;
+		
+		// Create a new event icon on the screen
+		GameObject eventIcon = GameObject.Instantiate(
+			eventIconPrefab,
+			new Vector2(0,0),
+			Quaternion.identity
+		) as GameObject;
+		eventIcon.transform.parent = transform;
+		eventIcon.transform.localPosition = new Vector3(
+			eventIcon.transform.localPosition.x,
+			eventIcon.transform.localPosition.y,
+			0
+		);
+		eventIcon.GetComponent<EventIcon>().eventLocation = position;
+		
+		events.Add(eventIcon);
+	}
+
+	// Use this for initialization
+	void Start () {
+		events = new List<GameObject>();
+		eventsToRemove = new List<GameObject>();
+		
+		// test
+		addEvent(new Vector2(-5,5));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Iterate over each of our events and display an icon for those happening off screen
+		foreach (GameObject eventIcon in events) {
+			if (isOnScreen(eventIcon.GetComponent<EventIcon>().eventLocation)) {
+				// If the event is on screen, remove it from the event list
+				//eventsToRemove.Add(eventIcon);
+			} else {
+				// If offscreen, display an icon that shows the user where to look
+				
+			}
+		}
+		
+		// Since we can't remove the elements from the list as we iterate, remove them after iterating
+		foreach (GameObject eventIcon in eventsToRemove) {
+			Debug.Log("Removing event at location: " + eventIcon.GetComponent<EventIcon>().eventLocation);
+			events.Remove(eventIcon);
+			Destroy(eventIcon);
+		}
+		eventsToRemove.Clear();
+	}
+	
+	private bool isOnScreen(Vector2 position) {
+		// Translate our given position to a viewport position (position relative to camera)
+		Vector2 viewport = Camera.main.WorldToViewportPoint(position);
+		
+		// If the viewport position is outside the bounds of the screen, it is considered 'offscreen'
+		if (viewport.x < 0 || viewport.x > 1) return false;
+		if (viewport.y < 0 || viewport.y > 1) return false;
+		
+		// Otherwise, it is considered onscreen
+		return true;
+	}
+}
