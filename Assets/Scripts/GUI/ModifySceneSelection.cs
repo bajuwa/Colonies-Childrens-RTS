f6876225def2c0ce7af5a34edafe1c9f192dae6e
bajuwa <justcallmebaj@gmail.com>
2014-11-23 11:54:18 -0800

    Updated gui with art and functionality

---
+++ Assets/Scripts/GUI/ModifySceneSelection.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModifySceneSelection : MonoBehaviour {
+
+	public int modifyBy;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnMouseOver() {
+		// If you want any hover effects, put them here!
+	
+		// If the user presses the button, load the next scene that is configured with this object
+		if (Input.GetMouseButtonDown(0)) { 
+			GameObject.Find("GameSetupManager").GetComponent<GameSetupManager>().changeMap(modifyBy);
+		}
+	}
+}
---
+++ Assets/Scripts/Managers/GameSetupManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameSetupManager : MonoBehaviour {
+
+	public List<Map> maps;
+	private int mapIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void changeMap(int modifyBy) {
+		mapIndex += modifyBy;
+		if (mapIndex > maps.Count - 1) mapIndex = 0;
+		if (mapIndex < 0) mapIndex = maps.Count - 1;
+		updateMapSettings();
+	}
+	
+	private void updateMapSettings() {
+		transform.Find("MapName").GetComponent<GUIText>().text = maps[mapIndex].name;
+		transform.Find("MapSelection").GetComponent<SpriteRenderer>().sprite = maps[mapIndex].scenePreview;
+		GameObject.Find("HostGame").GetComponent<ClickToLoadNextScene>().nextScene = (ClickToLoadNextScene.sceneName) maps[mapIndex].sceneId;
+	}
+	
+	[System.Serializable]
+	public class Map {
+		public string name;
+		public Sprite scenePreview;
+		public int sceneId;
+	}
+}
